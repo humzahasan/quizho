e2fa0e9adef4e0ad96d507ef5eca312c91f35d7d
Humza Hasan <humzahasan2101@gmail.com>
2022-05-24 23:37:57 +0530

    feat: header integrated

--- src/App.js
+++ src/App.js
@@ -1,7 +1,12 @@
 import "./App.css";
+import Header from "./component/Header/Header";
 
 function App() {
-  return <div className="app"></div>;
+  return (
+    <div className="app">
+      <Header />
+    </div>
+  );
 }
 
 export default App;
