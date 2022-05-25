61a47fc1bfbb07451011a25e316d6a538086f498
Humza Hasan <humzahasan2101@gmail.com>
2022-05-25 09:21:11 +0530

    fix: props passed to question card component

--- src/component/QuestionCard/QuestionCard.jsx
+++ src/component/QuestionCard/QuestionCard.jsx
@@ -1,19 +1,16 @@
 import React from "react";
 
-const QuestionCard = () => {
+const QuestionCard = ({ question, options, currentQuestion }) => {
   return (
     <div className="card card-lg">
       <div className="card-text">
-        <p className="card-subtitle">Question 1</p>
-        <p className="card-content">
-          Lorem ipsum dolor, sit amet consectetur adipisicing elit. Quis
-          voluptatum impedit rem, vero voluptatem tempora dolores! Sit nemo
-          officiis ab odit, sapiente nobis modi, neque reprehenderit blanditiis
-          vero voluptate amet!
-        </p>
+        <p className="card-subtitle">{currentQuestion}</p>
+        <p className="card-content">{question} </p>
       </div>
       <div className="card-action">
-        <button className="btn btn-outline btn-success">Option</button>
+        {options?.map((option) => (
+          <button className="btn btn-outline btn-success">{option}</button>
+        ))}
       </div>
     </div>
   );
