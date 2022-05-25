171c9f30bfec07e3359ecfe9a9abc50d97a06e4b
Humza Hasan <humzahasan2101@gmail.com>
2022-05-25 14:09:30 +0530

    feat: modified quiz card and question page

--- src/component/QuestionCard/QuestionCard.jsx
+++ src/component/QuestionCard/QuestionCard.jsx
@@ -1,16 +1,68 @@
 import React from "react";
+import Left from "../../assets/ArrowLeft.svg";
 
-const QuestionCard = ({ question, options, currentQuestion }) => {
+import Right from "../../assets/ArrowRight.svg";
+
+import "./QuestionCard.css";
+
+const QuestionCard = ({
+  currentQuestion,
+  totalQuestion,
+  prevQuestion,
+  nextQuestion,
+  currentQuestionIndex,
+}) => {
+  const isFirstQuestion = currentQuestionIndex === 0;
+  const isLastQuestion = currentQuestionIndex === totalQuestion - 1;
+
+  const { question, incorrectAnswers, correctAnswer } = currentQuestion;
+
+  const allOptions = [correctAnswer, ...incorrectAnswers].sort(
+    () => Math.random() - 0.5
+  );
+
+  console.log(question, incorrectAnswers, correctAnswer);
   return (
     <div className="card card-lg">
-      <div className="card-text">
-        <p className="card-subtitle">{currentQuestion}</p>
-        <p className="card-content">{question} </p>
+      <div className="card-badge">
+        <p className="card-badge-content">
+          Question {currentQuestionIndex + 1}/{totalQuestion}
+        </p>
       </div>
-      <div className="card-action">
-        {options?.map((option) => (
-          <button className="btn btn-outline btn-success">{option}</button>
+      <div className="card-text">
+        <p className="card-subtitle">{question}</p>
+      </div>
+      <div className="card-action-column">
+        {allOptions.map((option, index) => (
+          <button
+            disabled={currentQuestionIndex === 10}
+            key={option}
+            onClick={() => {}}
+            className="btn btn-outline btn-primary"
+          >
+            <p> {option} </p>
+          </button>
         ))}
+      </div>
+      <div className="card-extra-action">
+        <button
+          disabled={isFirstQuestion}
+          onClick={prevQuestion}
+          className={
+            isFirstQuestion ? "btn btn-disabled" : "btn btn-outline btn-primary"
+          }
+        >
+          <img src={Left} alt="left" />
+        </button>
+        <button
+          disabled={isLastQuestion}
+          onClick={nextQuestion}
+          className={
+            isLastQuestion ? "btn btn-disabled" : "btn btn-outline btn-primary"
+          }
+        >
+          <img src={Right} alt="right" />
+        </button>
       </div>
     </div>
   );
--- src/pages/Quiz/Quiz.jsx
+++ src/pages/Quiz/Quiz.jsx
@@ -1,8 +1,23 @@
-import React from "react";
+import React, { useState } from "react";
 import { Header, QuestionCard } from "../../component";
+import { useQuestion } from "../../context/QuestionContext";
 import "./Quiz.css";
 const Quiz = () => {
   const arr = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+  const { allQuestions } = useQuestion();
+  const [currentQuestionIndex, setCurrentQuestionIndex] = useState(0);
+  const prevQuestion = () => {
+    setCurrentQuestionIndex(currentQuestionIndex - 1);
+  };
+
+  const nextQuestion = () => {
+    setCurrentQuestionIndex(currentQuestionIndex + 1);
+  };
+
+  const goToQuestion = (item) => {
+    setCurrentQuestionIndex(item - 1);
+  };
+
   return (
     <div className="quizpage">
       <Header />
@@ -10,15 +25,25 @@ const Quiz = () => {
         <div className="quizpage-review">
           <div className="list list-vertical">
             {arr.map((item) => (
-              <p className="list-item">
+              <div
+                className="list-item"
+                key={item}
+                onClick={() => goToQuestion(item)}
+              >
                 <p className="sm-text">Question {item}</p>
                 <p className="cta-text">Answer {item}</p>
-              </p>
+              </div>
             ))}
           </div>
         </div>
         <div className="quizpage-question">
-          <QuestionCard />
+          <QuestionCard
+            prevQuestion={prevQuestion}
+            nextQuestion={nextQuestion}
+            currentQuestionIndex={currentQuestionIndex}
+            totalQuestion={allQuestions.length}
+            currentQuestion={allQuestions[currentQuestionIndex]}
+          />
         </div>
       </div>
     </div>
